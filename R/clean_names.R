cb289b07aa72b1cca641071df09188416b6438b6
ehwenk <ehwenk@gmail.com>
2023-07-03 17:28:26 +1000

    Import Lizzy's updated taxon_matching_functions

--- R/clean_names.R
+++ R/clean_names.R
@@ -469,15 +469,54 @@ update_taxonomy <- function(aligned_names,
 #' strip_names(c("Abies lasiocarpa subsp. lasiocarpa", "Quercus kelloggii", "Pinus contorta var. latifolia"))
 #'
 
-strip_names <- function(taxon_names) {
-  taxon_names %>%
-    stringr::str_remove_all(" subsp\\.") %>% stringr::str_remove_all(" aff\\.")  %>%
-    stringr::str_remove_all(" var\\.") %>% stringr::str_remove_all(" ser\\.") %>% stringr::str_remove_all(" f\\.") %>%
-    stringr::str_remove_all(" s\\.l\\.") %>% stringr::str_remove_all(" s\\.s\\.") %>%
-    stringr::str_replace_all("[-._()?]", " ") %>%
+strip_names <- function(x) {
+  x %>%
+    stringr::str_replace_all("[:punct:]", " ") %>%
+    stringr::str_replace_all(" subsp ", " ") %>%
+    stringr::str_replace_all(" var |var$", " ") %>%
+    stringr::str_replace_all(" ser ", " ") %>%
+    stringr::str_replace_all(" f ", " ") %>%
+    stringr::str_replace_all(" s l ", " ") %>%
+    stringr::str_replace_all(" s s ", " ") %>%
+    stringr::str_replace_all("\\=", " ") %>%
+    stringr::str_replace_all("  ", " ") %>%
     stringr::str_squish() %>%
     tolower()
 }
+
+
+#' Strip names of punctuation and filler words, including sp.
+#'
+#' @param x Text string, generally a taxonomic name
+#'
+#' @return Taxonomic name stripped of punctuation and filler words, including sp.
+#' @export
+#'
+#' @examples
+strip_names_2 <- function(x) {
+  x %>%
+    stringr::str_replace_all("[:punct:]", " ") %>%
+    stringr::str_replace_all(" subsp ", " ") %>%
+    stringr::str_replace_all(" var | var$", " ") %>%
+    stringr::str_replace_all(" ser ", " ") %>%
+    stringr::str_replace_all(" f ", " ") %>%
+    stringr::str_replace_all(" forma ", " ") %>%
+    stringr::str_replace_all(" species ", " ") %>%
+    stringr::str_replace_all(" s l ", " ") %>%
+    stringr::str_replace_all(" s s ", " ") %>%
+    stringr::str_replace_all(" ss ", " ") %>%
+    stringr::str_replace_all(" x ", " ") %>%
+    stringr::str_replace_all(" sp |sp $", " ") %>%
+    stringr::str_replace_all(" sp1", " 1") %>%
+    stringr::str_replace_all(" sp2", " 2") %>%
+    stringr::str_replace_all(" ssp |ssp $", " ") %>%
+    stringr::str_replace_all(" cf | cf$", " ") %>%
+    stringr::str_replace_all("\\=", " ") %>%
+    stringr::str_replace_all("  ", " ") %>%
+    stringr::str_squish() %>%
+    tolower()
+}
+
 
 
 #' Standardise Taxon Names
