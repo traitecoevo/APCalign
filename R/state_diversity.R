589201075b228889da4a505e0710792a4297f45f
Will Cornwell <w.cornwell@unsw.edu.au>
2023-07-06 17:22:44 +1000

    new function for nativeness

--- R/state_diversity.R
+++ R/state_diversity.R
@@ -176,13 +176,33 @@ get_apc_genus_family_lookup <-
   }
 
 
-#' @noRd
+#' Check If Species Is Native Anywhere In Australia
+#'
+#' This function checks if the given species is native anywhere in Australia.
+#' It creates a lookup table from taxonomic resources, and checks if the species
+#' is listed as native in that table.
+#'
+#' @param species A character string representing the binomial or common name of the species.
+#' @param resources An optional list of taxonomic resources to use for the lookup.
+#'        If not provided, the function will load default taxonomic resources using the `load_taxonomic_resources()` function.
+#' @return A tibble with two columns: `species`, which is the same as the input `species`,
+#'         and `native_anywhere_in_aus`, a logical vector indicating whether each species is native anywhere in Australia.
+#' @export
+#' @examples
+#' resources<-load_taxonomic_resources()
+#' native_anywhere_in_australia("Eucalyptus globulus")
+#' native_anywhere_in_australia(c("Eucalyptus globulus","Pinus radiata"), resources=resources)
+
 native_anywhere_in_australia <-
-  function(resources = load_taxonomic_resources()) {
+  function(species,resources = load_taxonomic_resources()) {
     native_lookup <-
       create_species_state_origin_matrix(resources = resources)
     native_lookup$native_anywhere <-
       apply(native_lookup, 1, function(x)
         any(grepl("native", x)))
-    return(native_lookup)
+    native_only<-filter(native_lookup,native_anywhere==TRUE) %>%
+      mutate(binomial=word(species,1,2))
+    return(tibble(species=species,
+                  native_anywhere_in_aus=species%in%native_only$binomial|
+                    species%in%native_only$species))
   }
