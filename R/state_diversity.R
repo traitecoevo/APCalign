f194027521332963d66383edea00ab37d2c3759e
Will Cornwell <w.cornwell@unsw.edu.au>
2023-07-31 12:15:47 +1000

    minor fixes to documentation

--- R/state_diversity.R
+++ R/state_diversity.R
@@ -166,8 +166,11 @@ get_apc_genus_family_lookup <-
 #'
 #' This function checks if the given species is native anywhere in Australia.
 #' It creates a lookup table from taxonomic resources, and checks if the species
-#' is listed as native in that table. Note that this will not detect within Australia invasives,
-#' e.g. if a species is from Western Australia and is invasive on the east coast.
+#' is listed as native in that table. Note that this will not detect within-Australia invasives,
+#' e.g. if a species is from Western Australia and is invasive on the east coast. 
+#' For the complete matrix of species by states that also represents within-Australia invasions, 
+#' use \link{create_species_state_origin_matrix}.
+#' 
 #'
 #' @family diversity methods
 #' @param species A character string representing the binomial or common name of the species.
@@ -177,9 +180,7 @@ get_apc_genus_family_lookup <-
 #'         and `native_anywhere_in_aus`, a logical vector indicating whether each species is native anywhere in Australia.
 #' @export
 #' @examples
-#' resources<-load_taxonomic_resources()
-#' native_anywhere_in_australia("Eucalyptus globulus")
-#' native_anywhere_in_australia(c("Eucalyptus globulus","Pinus radiata"), resources=resources)
+#' native_anywhere_in_australia(c("Eucalyptus globulus","Pinus radiata"))
 
 native_anywhere_in_australia <-
   function(species, resources = load_taxonomic_resources()) {
