56a203ee6b40af3b9cd0b3c6b76674b3d3b34095
Will Cornwell <w.cornwell@unsw.edu.au>
2023-08-03 15:08:31 +1000

    fixing #42

--- tests/testthat/test-state_diverstiy.R
+++ tests/testthat/test-state_diverstiy.R
@@ -1,5 +1,5 @@
 # ss<-create_species_state_origin_matrix(resources=resources)
-# 
+#
 # ss %>%
 #   slice(sample(1:n(),1000) %>%sort()) %>%
 #   write_csv("state_diversity.csv")
@@ -12,11 +12,26 @@ test_that("state_diversity() works", {
       sum(nsw_species_counts$num_species) < 10000
   )
   expect_error(state_diversity_counts(state = "NOTASTATE", resources = resources))
-  expect_equal(nrow(native_anywhere_in_australia(
-    c("Eucalyptus globulus", "Pinus radiata"), resources = resources
-  )), 2)
-  ss<-create_species_state_origin_matrix(resources=resources)
-  sd<-read_csv("state_diversity.csv")
-  ss_subset<-filter(ss,ss$species %in% sd$species)
-  expect_equal(ss_subset,sd)
+  ss <- create_species_state_origin_matrix(resources = resources)
+  sd <- read_csv("state_diversity.csv")
+  ss_subset <- filter(ss, ss$species %in% sd$species)
+  expect_equal(ss_subset, sd)
+})
+
+
+test_that("native_anywhere_in_australia() works", {
+  native_check <-
+    native_anywhere_in_australia(
+      c(
+        "Eucalyptus globulus",
+        "Pinus radiata",
+        "Brassica rapa",
+        "banksis notaspecies"
+      ),
+      resources = resources
+    )
+  #write_csv(native_check,"native_check.csv")
+  previous_check <- read_csv("native_check.csv")
+  expect_equal(native_check, previous_check)
+  expect_error(native_anywhere_in_australia(species = "NOTASPECIES", resources = resources))
 })
