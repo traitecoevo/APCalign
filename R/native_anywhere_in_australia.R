5af855468b9166b1d876c5e0b4c64052dd9eb1d2
Will Cornwell <wcornwell@gmail.com>
2023-08-03 15:47:41 +1000

    fixing #42 (#87)

--- R/native_anywhere_in_australia.R
+++ R/native_anywhere_in_australia.R
@@ -8,29 +8,38 @@
 #' use \link{create_species_state_origin_matrix}.
 #'
 #' @family diversity methods
-#' @param species A character string representing the binomial or common name of the species.
+#' @param species A character string representing the binomial for the species.  
 #' @param resources An optional list of taxonomic resources to use for the lookup.
 #'        If not provided, the function will load default taxonomic resources using the `load_taxonomic_resources()` function.
 #' @return A tibble with two columns: `species`, which is the same as the input `species`,
 #'         and `native_anywhere_in_aus`, a logical vector indicating whether each species is native anywhere in Australia.
 #' @export
 #' @examples
-#' \donttest{ native_anywhere_in_australia(c("Eucalyptus globulus","Pinus radiata"))}
+#' \donttest{native_anywhere_in_australia(c("Eucalyptus globulus","Pinus radiata","Banksis notaspecies"))}
 
 native_anywhere_in_australia <-
   function(species, resources = load_taxonomic_resources()) {
-    native_lookup <-
-      create_species_state_origin_matrix(resources = resources)
-    native_lookup$native_anywhere <-
-      apply(native_lookup, 1, function(x)
+    if(any(stringi::stri_count_words(species)!=2)){
+      stop("All input species needs to be a binomial for this function to work properly; \n  Consider using align_taxa first")
+    }
+    full_lookup <-
+      create_species_state_origin_matrix(resources = resources) %>%
+      dplyr::mutate(binomial = word(species, 1, 2))
+    
+    full_lookup$native_anywhere <-
+      apply(full_lookup, 1, function(x)
         any(grepl("native", x)))
-    native_only <- filter(native_lookup, native_anywhere == TRUE) %>%
-      mutate(binomial = word(species, 1, 2))
+    
+    native_only <- dplyr::filter(full_lookup, native_anywhere == TRUE)
+    
     return(
       tibble(
         species = species,
-        native_anywhere_in_aus = species %in% native_only$binomial |
-          species %in% native_only$species
+        native_anywhere_in_aus = dplyr::case_when(
+                         !species %in% full_lookup$binomial ~ "binomial not found in APC lookup",
+                         species %in% native_only$binomial ~ "considered native to Australia by APC",
+                         species %in% full_lookup$binomial ~ "not considered native to Australia by APC"
+                  )
       )
     )
   }
