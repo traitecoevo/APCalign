9aec8f08dc684aa8d1083bb896aa3e9d9c5b9662
ehwenk <ehwenk@gmail.com>
2023-08-14 09:13:22 +1000

    Update test-clean_names.R
    change test to eliminate APNI taxa, since by default APNI = FALSE

--- tests/testthat/test-clean_names.R
+++ tests/testthat/test-clean_names.R
@@ -131,12 +131,10 @@ test_that("handles weird strings", {
 })
 
 
-
-
-test_that("handles APNI taxa and genus level IDs",{
-  zz<-create_taxonomic_update_lookup(c("Acacia sp.", "Dendropanax amplifolius",
-                                      "Acanthopanax divaricatum", "Eucalyptus sp."), resources=resources)
-  expect_gte(nrow(zz), 4)
-})
+  test_that("handles APNI taxa and genus level IDs",{
+    zz<-create_taxonomic_update_lookup(c("Acacia sp.", "Dendropanax amplifolius",
+                                        "Acanthopanax divaricatum", "Eucalyptus sp."), resources=resources)
+    expect_gte(nrow(zz), 2)
+  })
 
 
