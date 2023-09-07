73523624fa2477b6f6302d2bb8a2be144f39d6b8
Will Cornwell <w.cornwell@unsw.edu.au>
2023-09-07 11:23:04 +1000

    Better handling of edge cases in native/introduced function

--- R/native_anywhere_in_australia.R
+++ R/native_anywhere_in_australia.R
@@ -8,38 +8,55 @@
 #' use \link{create_species_state_origin_matrix}.
 #'
 #' @family diversity methods
-#' @param species A character string representing the binomial for the species.  
+#' @param species A character string typically representing the binomial for the species.  
 #' @param resources An optional list of taxonomic resources to use for the lookup.
 #'        If not provided, the function will load default taxonomic resources using the `load_taxonomic_resources()` function.
-#' @return A tibble with two columns: `species`, which is the same as the input `species`,
+#' @return A tibble with two columns: `species`, which is the same as the unique values of the input `species`,
 #'         and `native_anywhere_in_aus`, a logical vector indicating whether each species is native anywhere in Australia.
 #' @export
 #' @examples
 #' \donttest{native_anywhere_in_australia(c("Eucalyptus globulus","Pinus radiata","Banksis notaspecies"))}
 
-native_anywhere_in_australia <-
-  function(species, resources = load_taxonomic_resources()) {
-    if(any(stringi::stri_count_words(species)!=2)){
-      stop("All input species needs to be a binomial for this function to work properly; \n  Consider using align_taxa first")
-    }
-    full_lookup <-
-      create_species_state_origin_matrix(resources = resources) %>%
-      dplyr::mutate(binomial = word(species, 1, 2))
-    
-    full_lookup$native_anywhere <-
-      apply(full_lookup, 1, function(x)
-        any(grepl("native", x)))
-    
-    native_only <- dplyr::filter(full_lookup, native_anywhere == TRUE)
-    
-    return(
-      tibble(
-        species = species,
-        native_anywhere_in_aus = dplyr::case_when(
-                         !species %in% full_lookup$binomial ~ "binomial not found in APC lookup",
-                         species %in% native_only$binomial ~ "considered native to Australia by APC",
-                         species %in% full_lookup$binomial ~ "not considered native to Australia by APC"
-                  )
-      )
-    )
+native_anywhere_in_australia <- function(species, resources = load_taxonomic_resources()) {
+  
+  # Remove duplicates and replace hyphens
+  species <- unique(species)
+  species_modified <- gsub('-', ' ', species)
+  
+  # Count words and issue warnings for non-binomial names
+  word_counts <- stringi::stri_count_words(species_modified)
+  if (any(word_counts != 2)) {
+    warning("All input species not binomials; \n  Consider using align_taxa first")
   }
+  
+  # Create lookup tables
+  full_lookup <- create_species_state_origin_matrix(resources = resources)
+  
+  # Filter for native species
+  full_lookup$native_anywhere <-
+    apply(full_lookup, 1, function(x)
+      any(grepl("native", x)))
+  native_only<-dplyr::filter(full_lookup,native_anywhere)
+  
+  # Get the first two words (assuming these are the binomial names)
+  natives_binomials <- stringr::word(native_only$species, 1, 2)
+  full_binomials <- stringr::word(full_lookup$species, 1, 2)
+  species_binomials <- stringr::word(species, 1, 2)
+  
+  # Check membership
+  natives <- species_binomials %in% natives_binomials
+  fulllist <- species_binomials %in% full_binomials
+  
+  # Create output tibble
+  result <- tibble(
+    species = species,
+    native_anywhere_in_aus = dplyr::case_when(
+      natives & fulllist ~ "native",
+      fulllist ~ "introduced",
+      TRUE ~ "unknown"
+    )
+  )
+  
+  return(result)
+}
+
