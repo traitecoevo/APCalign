836c475c1c32680d5aea6078c16c6a0356966881
Fonti Kar <f.kar@unsw.edu.au>
2023-10-19 16:30:03 +1100

    rm else

--- R/load_taxonomic_resources.R
+++ R/load_taxonomic_resources.R
@@ -273,7 +273,7 @@ dataset_access_function <-
     if (!curl::has_internet() | !network) { # Simulate if network is down
       message("No internet connection, please retry with stable connection (dataset_access_function)")
       return(invisible(NULL))
-    } else {
+    } 
     
     # Download from Github Release
     if (type == "stable") {
