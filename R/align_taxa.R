71eed1aa492e1c8a7043ddd11c3dc742286a09b7
Will Cornwell <w.cornwell@unsw.edu.au>
2024-04-21 16:40:09 +1000

    checking cached file

--- R/align_taxa.R
+++ R/align_taxa.R
@@ -90,11 +90,16 @@ align_taxa <- function(original_name,
           .default = readr::col_character()
         )
       )
-    
-    #
-    # TODO: check taxa_ raw has correct columns
-    # IT DOESN'T
-    #
+    correct_names <- c("original_name", "aligned_name", "accepted_name", "suggested_name", 
+                           "genus", "family", "taxon_rank", "taxonomic_dataset", "taxonomic_status", 
+                           "taxonomic_status_aligned", "aligned_reason", "update_reason", 
+                           "subclass", "taxon_distribution", "scientific_name", "taxon_ID", 
+                           "taxon_ID_genus", "scientific_name_ID", "canonical_name", "row_number", 
+                           "number_of_collapsed_taxa", "checked", "known")
+    if(!identical(names(taxa_raw), correct_names)) {
+      stop("Your output file already exists and it's not in the right format. 
+         Please check that the file you are passing in to the output option.")
+    }
   }
   else {
     taxa_raw <-
@@ -160,22 +165,22 @@ align_taxa <- function(original_name,
   # move all checked taxa to "checked"
   taxa <- redistribute(taxa)
   
-  if (!is.null(output) && file.exists(output)) {
-  # check unknown taxa
-  message(
-    "  -> ",
-    crayon::blue(sum(taxa$known, na.rm = T)),
-    " names already matched; ",
-    crayon::blue(sum(
-      taxa$checked &
-        !taxa$tocheck$known,
-      na.rm = T
-    )),
-    " names checked but without a match; ",
-    crayon::blue(sum(!taxa$tocheck$checked)),
-    " taxa yet to be checked"
-  )
-  }
+  # if (!is.null(output) && file.exists(output)) {
+  # # check unknown taxa
+  # message(
+  #   "  -> ",
+  #   crayon::blue(sum(taxa$known, na.rm = T)),
+  #   " names already matched; ",
+  #   crayon::blue(sum(
+  #     taxa$checked &
+  #       !taxa$tocheck$known,
+  #     na.rm = T
+  #   )),
+  #   " names checked but without a match; ",
+  #   crayon::blue(sum(!taxa$tocheck$checked)),
+  #   " taxa yet to be checked"
+  # )
+  # }
   # do the actual matching
   taxa <- 
     match_taxa(taxa, resources, fuzzy_abs_dist, fuzzy_rel_dist, fuzzy_matches, imprecise_fuzzy_matches, APNI_matches, identifier) %>%
@@ -206,7 +211,7 @@ align_taxa <- function(original_name,
     taxa$checked<-TRUE
     taxa$known<-!is.na(taxa$aligned_name)
     readr::write_csv(taxa, output)
-    message("  - output saved in file: ", output)
+    #message("  - output saved in file: ", output)
   }
 
   return(taxa)
