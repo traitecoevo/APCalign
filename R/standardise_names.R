8b121a665998973c146603190278254771d645e1
Elizabeth Wenk <ehwenk@gmail.com>
2026-03-23 15:03:14 +1100

    Update standardise_names.R
    remove `(` at start of genus names. Relevant for some hybrids of hybrid names

--- R/standardise_names.R
+++ R/standardise_names.R
@@ -146,7 +146,10 @@ extract_genus <- function(taxon_name) {
   
   taxon_name <- standardise_names(taxon_name)
 
-  genus <- stringr::str_split_i(taxon_name, " |\\/", 1) %>% stringr::str_to_sentence()
+  genus <- 
+    stringr::str_split_i(taxon_name, " |\\/", 1) |>
+      stringr::str_to_sentence() |> 
+      stringr::str_replace("^\\(", "")
   
   # Deal with names that being with x, 
   # e.g."x Taurodium x toveyanum" or "x Glossadenia tutelata"
