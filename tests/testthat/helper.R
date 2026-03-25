a4710d447c4bbc23069f9cf850d98f1c5eb82840
Will Cornwell <w.cornwell@unsw.edu.au>
2026-03-25 21:03:39 +1100

    removing a major source of confusion

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,1 @@
-# hack to enable faster load times for tests
-if(exists("resources", envir = globalenv())) {
-  resources <- get("resources", envir = globalenv())
-} else {
-  resources <- load_taxonomic_resources(stable_or_current_data = "stable", version = "2024-10-11")
-}
+resources <- load_taxonomic_resources(stable_or_current_data = "stable", version = "2024-10-11")
